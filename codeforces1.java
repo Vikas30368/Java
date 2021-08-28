aaa0cdeed28167d973b5612980252b9087a5287f
Vikas30368 <80503447+Vikas30368@users.noreply.github.com>
2021-08-28 09:36:11 +0530

    Add files via upload

---
+++ codeforces1.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int w[100];
+		if(w%2==0)
+		{
+		    System.out.println("YES");
+		}
+		else
+		{
+		    System.out.println("NO");
+		}
+	}
+}
